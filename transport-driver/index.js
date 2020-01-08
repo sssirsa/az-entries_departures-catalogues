e306c5b001a75f382c0f55e5c4d97f4dfe40caf4
Platae <platae93@gmail.com>
2020-01-07 21:35:55 -0600

    CRUD base de Operadores de transporte

--- transport-driver/index.js
+++ transport-driver/index.js
@@ -1,16 +1,305 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const mongodb = require('mongodb');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+let cosmos_client = null;
+
+const connection_cosmosDB = process.env["connection_cosmosDB"];
+const connection_storage = process.env["storageentriesdepartures_STORAGE"];
+
+module.exports = function (context, req) {
+    //Create transport kind
+    if (req.method === "POST") {
+        var transportLineId = req.body['linea_transporte_id'];
+        var idFront = req.body['identificacion_anverso'];
+        var idRear = req.body['identificacion_reverso'];
+        //Transport driver object building
+        var newTransportDriver = {
+            nombre: req.body.nombre,
+            identificacion_anverso: null,
+            identificacion_reverso: null
         };
+        //Rejecting the request if the minimum fields are not received
+        if (!transportLineId || !idFront || !newTransportDriver.nombre) {
+            context.res = {
+                status: 400,
+                body: 'Required fields: "nombre", "linea_transporte_id", "identificacion_anverso" ',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+            context.done();
+        }
+        //Search transport line and then add it to transport line object
+        createCosmosClient()
+            .then(function () {
+                searchTransportLine(transportLineId)
+                    .then(function (transportLine) {
+                        if (transportLine) {
+                            newTransportDriver['linea_transporte'] = transportLine;
+                            //Write the transport line to the database
+                            createCosmosClient()
+                                .then(function () {
+                                    writeTransportDriver(newTransportDriver)
+                                        .then(function (transportDriver) {
+                                            context.res = {
+                                                status: 201,
+                                                body: transportDriver.ops[0],
+                                                headers: {
+                                                    'Content-Type': 'application/json'
+                                                }
+                                            };
+                                            context.done();
+                                        })
+                                        .catch(function (error) {
+                                            context.log('Error writing the transport driver to the database');
+                                            context.log(error);
+                                            context.res = { status: 500, body: error };
+                                            context.done();
+                                        });
+                                })
+                                .catch(function (error) {
+                                    context.log('Error creating cosmos_client for transport driver creation');
+                                    context.log(error);
+                                    context.res = { status: 500, body: error };
+                                    context.done();
+                                });
+                        }
+                        else {
+                            context.log('No transport line found with the given id')
+                            context.res = {
+                                status: 400,
+                                body: { message: "ES-044" },
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                }
+                            };
+                            context.done();
+                        }
+                    })
+                    .catch(function (error) {
+                        context.log('Error searching transport line');
+                        context.log(error);
+                        context.res = { status: 500, body: error };
+                        context.done();
+                    });
+            })
+            .catch(function (error) {
+                context.log('Error creating cosmos_client for transport line search');
+                context.log(error);
+                context.res = { status: 500, body: error };
+                context.done();
+            });
+
+
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
+
+    //Get transport lines
+    if (req.method === "GET") {
+        var requestedID;
+        var filter;
+        if (req.query) {
+            requestedID = req.query["id"];
+            filter = req.query["filter"];
+        }
+        if (requestedID) {
+            //Search for one transport line
+            createCosmosClient()
+                .then(function () {
+                    getTransportDriver(requestedID)
+                        .then(function (transportDriver) {
+                            context.res = {
+                                status: 200,
+                                body: transportDriver,
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                }
+                            };
+                            context.done();
+                        })
+                        .catch(function (error) {
+                            context.log('Error reading transport driver from database');
+                            context.log(error);
+                            context.res = { status: 500, body: error };
+                            context.done();
+                        });
+                })
+                .catch(function (error) {
+                    context.log('Error creating cosmos_client for transport driver detail');
+                    context.log(error);
+                    context.res = { status: 500, body: error };
+                    context.done();
+                });
+        }
+        else {
+            createCosmosClient()
+                .then(function () {
+                    getTransportDrivers(filter)
+                        .then(function (transportDrivers) {
+                            context.res = {
+                                status: 200,
+                                body: transportDrivers,
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                }
+                            };
+                            context.done();
+                        })
+                        .catch(function (error) {
+                            context.log('Error reading transport drivers from database');
+                            context.log(error);
+                            context.res = { status: 500, body: error };
+                            context.done();
+                        });
+                })
+                .catch(function (error) {
+                    context.log('Error creating cosmos_client for transport drivers list');
+                    context.log(error);
+                    context.res = { status: 500, body: error };
+                    context.done();
+                });
+        }
     }
+
+    //Delete transport line
+    if (req.method === "DELETE") {
+        var requestedID;
+        if (req.query) {
+            requestedID = req.query["id"];
+        }
+        if (requestedID) {
+            //Search for one transport line
+            createCosmosClient()
+                .then(function () {
+                    deleteTransportDriver(requestedID)
+                        .then(function () {
+                            context.res = {
+                                status: 204,
+                                body: {}
+                            };
+                            context.done();
+                        })
+                        .catch(function (error) {
+                            context.log('Error deleting transport driver from database');
+                            context.log(error);
+                            context.res = { status: 500, body: error };
+                            context.done();
+                        });
+                })
+                .catch(function (error) {
+                    context.log('Error creating cosmos_client for transport driver deletion');
+                    context.log(error);
+                    context.res = { status: 500, body: error };
+                    context.done();
+                });
+        }
+        else {
+            context.res = {
+                status: 400,
+                body: 'The parameter "id" was not found on the request'
+            };
+            context.done();
+        }
+    }
+
+    function createCosmosClient() {
+        return new Promise(function (resolve, reject) {
+            if (!cosmos_client) {
+                mongodb.MongoClient.connect(connection_cosmosDB, function (error, _cosmos_client) {
+                    if (error) {
+                        reject(error);
+                    }
+                    cosmos_client = _cosmos_client;
+                    resolve();
+                });
+            }
+            else {
+                resolve();
+            }
+        });
+    }
+
+    function searchTransportLine(transportLineId) {
+        return new Promise(function (resolve, reject) {
+            cosmos_client
+                .db('EntriesDepartures')
+                .collection('TransportLine')
+                .findOne({ _id: mongodb.ObjectId(transportLineId) },
+                    function (error, docs) {
+                        if (error) {
+                            reject(error);
+                        }
+                        resolve(docs);
+                    }
+                );
+        });
+    }
+
+    function writeTransportDriver(transportLine) {
+        // Write the entry to the database.
+        return new Promise(function (resolve, reject) {
+            cosmos_client
+                .db('EntriesDepartures')
+                .collection('TransportDriver')
+                .insertOne(transportLine,
+                    function (error, docs) {
+                        if (error) {
+                            reject(error);
+                        }
+                        resolve(docs);
+                    }
+                );
+        });
+    }
+
+    function getTransportDriver(transportLineId) {
+        return new Promise(function (resolve, reject) {
+            cosmos_client
+                .db('EntriesDepartures')
+                .collection('TransportDriver')
+                .findOne({ _id: mongodb.ObjectId(transportLineId) },
+                    function (error, docs) {
+                        if (error) {
+                            reject(error);
+                        }
+                        resolve(docs);
+                    }
+                );
+        });
+    }
+
+    function getTransportDrivers(query) {
+        return new Promise(function (resolve, reject) {
+            cosmos_client
+                .db('EntriesDepartures')
+                .collection('TransportDriver')
+                .find(query)
+                .toArray(function (error, docs) {
+                    if (error) {
+                        reject(error);
+                    }
+                    resolve(docs)
+                });
+        });
+    }
+
+    function deleteTransportDriver(transportLineId) {
+        return new Promise(function (resolve, reject) {
+            cosmos_client
+                .db('EntriesDepartures')
+                .collection('TransportDriver')
+                .deleteOne({ _id: mongodb.ObjectId(transportLineId) },
+                    function (error, docs) {
+                        if (error) {
+                            reject(error);
+                        }
+                        resolve(docs);
+                    }
+                );
+        });
+    }
+
+    function writeBlob(base64Image) {
+        //const blobServiceClient = await BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+
+    }
+
 };
